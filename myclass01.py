f9557dbabf84ef1ce719b4bbd5e01dc0e06b8ef0
hafizmuhammadyahya123 <hafizmuhammadyahya123@gmail.com>
2025-08-18 22:52:01 +0500

    local-remote

---
+++ myclass01.py
@@ -0,0 +1,116 @@
+# print() statement
+print("hello")
+
+# multiline string use to triple qotataion
+print("""
+      python is heart of 
+      artificial; intelligence
+      and generative AI is a 
+      part of deep learning..
+      """)
+
+print('''surrogate end points basically 
+      aik example k thhrough samajhtay hain 
+      suppose aik Heart Patient ha 
+      ab agr wo doctor k pss jata ha 
+      or is ka test kr k medicine recommanded
+      kr di jae or kaha jae daily ye use krty
+      rahy tou ye absolutely wrong ha..
+      Treatment tou ye ha k kuch surrogate
+      end poijnts build kre means actual 
+      end point jesy Heart Attack ye actual
+      end point tha ab is sy pehly end point jesy 
+      CHOLESTROL, BLOOD PRESSURE ye two surrogate 
+      end points build kiyay takay jo medicine refer 
+      ki thi kiya wo effected ha bhi ya nahi agr ha 
+      NOW MEDICINE CONTINUE RKHNA HA AGR NAHI TOU KOE OR TREATMENT
+      YA MEDICINE CHANGE KRNAY KI NEED HA AGR HUM THIS PARTICULAR CASE 
+      MAIN YE 2 SURROGATE END POINTS NAHI BNAE TOU TO WO ACTUAL END POINT MEANS
+      HEART ATTACK IS K REHAM O KARAM PR CHOR DIYA JAE SIRF MEDICINE REFER KR K TOU WO 
+      PATIENT JALD HI MOUT K MOUN MAIN JA SKTA HA THIS IS ABSOLUTELY WRONGE...''')
+
+# VARIABLES FOR STRING:-
+student  = 'Aaliyan'
+print(student)
+
+student_name = 'IMTIAZ' #SNAKE CASE VAR NAME
+print(student_name)
+
+StudentName = 'Aaliyan' #PASCAL CASE VAR NAME
+print(StudentName)
+
+new_student_name = student +" "+ student_name #concatenate
+print(new_student_name)
+
+new_student_name = student_name + student
+print(new_student_name)
+
+print(type(new_student_name))
+
+int_convert_str = '100' #aik var name data type allowed
+print(f"This is string :{int_convert_str}")
+print(type(int_convert_str))
+
+# VARIABLE FOR int:-
+int = 100 #aik var name data type allowed
+print(int)
+
+# and = 100  #aik keyword ko as a var use krna not allowed
+# print(and)
+
+int_num = 3
+print(int_num)
+print(type(int_num))
+
+RandomNumber = 50
+print(RandomNumber)
+
+num1,num2,num3,num4 = 100,150,200,300
+print(num3)
+
+summation = num3 + num4
+print(summation)
+
+num1, num2, num3, num4 = "100", "200", "300", "400"  #not  number 
+print(num1)
+print(type(num1))
+
+concatenate = num1+num4 #this is called concatenate
+print(concatenate)
+print(type(concatenate))
+
+num1 = num2 = num3 = 5000 
+print(num3)
+print(num1)
+
+num1 = num2 = num3 = (1000 * 5)
+print(num3)
+
+# num4 = num1 + '1000' #Type Error
+# print(num4)
+
+# MATH EXPRESSION : FAMILIAR OPERATOR:-
+plus = 2 + 10
+print(plus)
+
+difference = (100-50-50)
+print(difference)
+print(type(difference))
+
+product = 2 * 9
+print(product)
+
+quotient = 10/2
+print(quotient)
+
+exponnent = 2 ** 4
+print(exponnent)
+
+floor_division =  10 // 5 # without decimal output
+print(floor_division)
+
+quotient = 10 / 5 # decimal output
+print(quotient)
+
+
+
