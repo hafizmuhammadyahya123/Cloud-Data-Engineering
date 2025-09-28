ca05dcc0f2b54cbc3f1fd12cdf1d89534d10b175
hafizmuhammadyahya123 <hafizmuhammadyahya123@gmail.com>
2025-09-28 22:51:17 +0500

    Create modules.py

---
+++ python/modules.py
@@ -0,0 +1,8 @@
+def add_num(num1 , num2):
+    return num1 + num2
+
+# ---------------------------------------------------
+
+def my_number_display(int1 , int2 , int3):
+
+    return lambda a , b : (a * int1)/b + int2**int3
